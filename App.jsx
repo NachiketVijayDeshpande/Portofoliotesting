bb1c457d1487339111babb4d7cbd76040fe952e5
NachiketVijayDeshpande <156070907+NachiketVijayDeshpande@users.noreply.github.com>
2024-09-27 11:30:31 +0100

    commit

--- App.jsx
+++ App.jsx
@@ -1,9 +1,36 @@
-import React from "react"
+import React,{ useRef } from 'react'
+import About from "/Components/aboutme.jsx"
+import TopButton from "/Components/TopButton.jsx"
+import Projects from "/Components/Projects.jsx"
+import Firstpage from "/Components/FirstPage.jsx"
+import Social from "/Components/Socialmedia.jsx"
+import Form from "/Components/Form.jsx"
+import Footer from "/Components/Footer.jsx"
 
-export default function App(){
+
+
+export default function App() {
+    const[showdetail,setShowdetail] =React.useState(false)
+    
+    function toggleDetail(){
+        setShowdetail(prev=>!prev)
+    }
+    
+    function setToggleOf(){
+        setShowdetail(prev=>false)
+    }
+    
+   
   return (
-    <div>
-    <h1>Hello World</h1>
+    <div className="main" style={{ minHeight: "200vh" }}>
+        <TopButton toggleDetail={toggleDetail}  setToggleOf={setToggleOf} />
+       <Firstpage/>
+       <Social/>
+    { showdetail &&<About toggle={toggleDetail}/>}
+    <Projects/>
+    <Form  />
+    <Footer/>
     </div>
   )
 }
+
