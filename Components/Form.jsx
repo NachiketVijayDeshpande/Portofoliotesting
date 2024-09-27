65736d6fe3ce9551baa0c0e8b765a9eaca7f952b
NachiketVijayDeshpande <156070907+NachiketVijayDeshpande@users.noreply.github.com>
2024-09-27 14:39:39 +0100

    submit

--- Components/Form.jsx
+++ Components/Form.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import introImage from '../images/image.png';  // Adjust paths according to your structure
 import profileImage from '../images/Profileimage.jpeg';
+
+
 const Form = React.forwardRef((props, ref) => {
+  const [submit,setSubmit]= React.useState(false)
+  function submitbtn(e){
+    e.reventDefault()
+    setSubmit(true)
+  }
   return (
     <div className="formDiv">
       <h1 className="formheead">Form</h1>
           <img id="form-img" src={profileImage}/>
           <h2>NACHIKET</h2>
+          {submit&&<h2>FORM SUBMITTED</h2>}
           <span className="">
-            <a href="https://www.linkedin.com/in/nachiket-vijay-deshpande/">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/nachiket-vijay-deshpande/">Linkedin</a>
           </span>
-            <form className="formsection" netlify >
+            <form className="formsection" netlify onSubmit={submitbtn} >
               <div className="inputdiv">
                 <input type="text" required placeholder="Name" aria-label="Name" />
                 <input type="email" required placeholder="Email" aria-label="Email" />
@@ -18,8 +26,9 @@ const Form = React.forwardRef((props, ref) => {
               <input type="text" required placeholder="Subject" aria-label="Email" />
               <input id="message" type="text" required placeholder="Message" aria-label="Email" />
                 <br />
-                <button className="formbtn" type="submit">Submit</button>
-                <p className="fine-print">Get in touch</p>
+                <button className="formbtn"  type="submit">Submit</button>
+                {!submit&&<p className="fine-print">Get in touch</p>}
+                
             </form>
        
     </div>
