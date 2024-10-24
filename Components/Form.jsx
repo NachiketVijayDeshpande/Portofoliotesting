383cf5056936c4f437cdef26d55185b99b65e217
NachiketVijayDeshpande <156070907+NachiketVijayDeshpande@users.noreply.github.com>
2024-10-24 08:24:45 +0100

    Update Form.jsx

--- Components/Form.jsx
+++ Components/Form.jsx
@@ -4,11 +4,31 @@ import profileImage from '../images/Profileimage.jpeg';
 
 
 const Form = React.forwardRef((props, ref) => {
+  const [result, setResult] = React.useState("");
   const [submit,setSubmit]= React.useState(false)
-  function submitbtn(e){
-    e.reventDefault()
-    setSubmit(true)
-  }
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "cbc82cd7-cef8-469a-b7fb-7886f36cd2cc");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className="formDiv">
       <h1 className="formheead">Form</h1>
@@ -18,7 +38,7 @@ const Form = React.forwardRef((props, ref) => {
           <span className="">
             <a href="https://www.linkedin.com/in/nachiket-vijay-deshpande/">Linkedin</a>
           </span>
-            <form className="formsection" netlify onSubmit={submitbtn} >
+            <form className="formsection" onSubmit={onSubmit} >
               <div className="inputdiv">
                 <input type="text" required placeholder="Name" aria-label="Name" />
                 <input type="email" required placeholder="Email" aria-label="Email" />
